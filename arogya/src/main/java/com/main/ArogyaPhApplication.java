00eb0b0b6b6668f341050c4c4533b4cd0903218c
nvleensingh <navleensingh19@gmail.com>
2021-02-27 10:46:54 +0530

    swagger

--- arogya/src/main/java/com/main/ArogyaPhApplication.java
+++ arogya/src/main/java/com/main/ArogyaPhApplication.java
@@ -4,9 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication(scanBasePackages = "com")
 @EntityScan(basePackages = "com.bean")
 @EnableJpaRepositories(basePackages = "com")
+@EnableSwagger2
 public class ArogyaPhApplication {
 
 	public static void main(String[] args) {
