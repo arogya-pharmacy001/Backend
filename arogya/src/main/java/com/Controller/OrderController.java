bc5676cda5f62847dea53b8366a4da626a368a2a
nvleensingh <navleensingh19@gmail.com>
2021-03-03 00:16:27 +0530

    order high functionality

--- arogya/src/main/java/com/Controller/OrderController.java
+++ arogya/src/main/java/com/Controller/OrderController.java
@@ -3,6 +3,7 @@ package com.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.web.SortDefault;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -52,4 +53,23 @@ public class OrderController {
 	{
 		return OS.deleteOrder(order_id);
 	}
+	
+	@GetMapping(value="displayquan",produces=MediaType.APPLICATION_JSON_VALUE)
+	public List<orders> getAllOrderByQuantity()
+	{
+		return OS.getAllOrdersByQuantity();
+	}
+	@GetMapping(value="displaydate",produces=MediaType.APPLICATION_JSON_VALUE)
+	public List<orders> getAllOrderByDate()
+	{
+		return OS.getAllOrdersByDate();
+	}
+	
+	@GetMapping(value="month/{year}/{month}", produces=MediaType.APPLICATION_JSON_VALUE)
+	public List<orders> getAllOrdersSaleByMonth(@PathVariable("year") int year,@PathVariable("month") int month )
+	{
+		return OS.getAllOrdersSaleByMonth(year,month);
+	}
+
 }
+
--- arogya/src/main/java/com/service/orderService.java
+++ arogya/src/main/java/com/service/orderService.java
@@ -1,19 +1,29 @@
 package com.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.SortDefault;
 import org.springframework.stereotype.Service;
 
 import com.bean.orders;
 import com.dao.OrdersRepository;
+import com.dao.orderDao;
 
 
 @Service
 public class orderService {
 	@Autowired
 	OrdersRepository OR;
+	orders od;
+	
+	@Autowired
+	orderDao ordao;
 	
 	public List<orders> displayAllOrders()
 	{
@@ -61,5 +71,54 @@ public class orderService {
 			return "id not found";
 		}
 	}
+	
+
+	public List<orders> getAllOrdersByQuantity()
+	{
+		 return null;
+			
+		
+      //  return OR.findAll().stream().sorted().collect(Collectors.toList());
+		
+	//return OR.findAll().stream().filter(ord->ord.getAmount()>ord.getAmount()).collect(Collectors.toList());
+		     
+		//	return OR.findAll().stream().sorted().
+	
+		 
+	//	 return OR.findAll().stream().sorted(
+		 
+		 
+		 
+	//return OR.findAll().sort(null);	
+	
+	//	return OR.findAll(Sort.by(od.getItems()));
+//return OR.findAll().stream().filter().collect(Collectors.toCollection(ArrayList::new));	
+	/// galatreturn OR.findAll().sort(Sort.Order.by(od.getItems())));
+	/*
+	List<orders> ordrec= new ArrayList<>();
+	
+	OR.findAll().forEach(ordrec::add);
+	*/
+		
+		
+		
+		
+	
+	}
+	
+	
+	public List<orders> getAllOrdersByDate()
+	{
+		return null;
+	}
+	
+	
+	public List<orders> getAllOrdersSaleByMonth(int year, int month)
+	{
+		return null;
+	}
+	
+	
+	
 	
 }
