116c7816d1315b3c53e0f005059f0a19a1af1a91
nvleensingh <navleensingh19@gmail.com>
2021-03-03 00:30:03 +0530

    order high functionality

--- arogya/src/main/java/com/dao/orderDao.java
+++ arogya/src/main/java/com/dao/orderDao.java
@@ -22,7 +22,7 @@ public class orderDao {
 	public List<orders> getAllOrdersByQuantity()
 	{
 		EntityManager emf = factory.createEntityManager();
-		Query qry = emf.createNativeQuery(null);
+		Query qry = emf.createNativeQuery("select * from orders order by items desc");
 		
 		
 		List<orders> list = qry.getResultList();
@@ -33,7 +33,7 @@ public class orderDao {
 	public List<orders> getAllOrdersByDate()
 	{
 		EntityManager em = factory.createEntityManager();
-		Query qry = em.createNativeQuery(null);
+		Query qry = em.createNativeQuery("select * from orders order by order_date desc");
 		
 		List<orders> list = qry.getResultList();
 		return list;
@@ -43,7 +43,9 @@ public class orderDao {
 	public List<orders> getAllOrdersSaleByMonth(int year, int month)
 	{
 		EntityManager em = factory.createEntityManager();
-		Query qry = em.createNativeQuery(null);
+		Query qry = em.createQuery("select p from orders where p.order_date like 'year-month-%'");
+		
+		//select * from orders where order_date like '2020-12-%'
 		
 		List<orders> list = qry.getResultList();
 		return list;
